2a5225dfc306e07b5454633d49891b31c34eb90d
Robert Moore <me@robdmoore.id.au>
2012-08-27 23:27:29 +0800

    Empty webapi project minus all the mvc stuff

---
+++ Hal.PlayAround/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace Hal.PlayAround
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+    }
+}
